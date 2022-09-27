850eebb166696e511c6636bfde41ebe1d54eba50
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2022-09-27 10:35:21 +0900

    20220927
    ベースプロジェクト

---
+++ Assets/Scripts/Poi/PoiMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoiMove : MonoBehaviour
+{
+    //Animatorをanimという変数で定義する
+    private Animator anim;
+    private float mTime;
+    private float mScoopTime;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+        //変数animに、Animatorコンポーネントを設定する
+        anim = gameObject.GetComponent<Animator>();
+        mTime = 0.0f;
+        mScoopTime = 15.0f;
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        mTime += Time.deltaTime;
+        // 時間経過したら
+        if (mTime > mScoopTime)
+        {
+            // Bool型のパラメーターをTrueにする
+            anim.SetBool("scoop", true);
+            mTime = 0.0f;
+            mScoopTime -= 1;
+        }
+        else
+        {
+            // bool型のパラメーターをfalseにする
+            anim.SetBool("scoop", false);
+        }
+    }
+}
